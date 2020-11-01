58d9fd202906b0701e3fe252824ac758a48f080d
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-01 15:09:11 -0500

    Compelted 6th query

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -34,3 +34,13 @@ SELECT first_name, last_name, sex
 FROM employees 
 WHERE first_name = 'Hercules'
 AND last_name LIKE 'B%';
+
+
+-- List all employees in the Sales department, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de
+ON de.emp_no = e.emp_no
+INNER JOIN departments d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales';
