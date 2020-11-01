ea9c3a9f045abac2d9000f1df531a432d809e7c9
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-01 13:50:37 -0500

    edited ERD image and uploaded table schema

---
+++ table_schemata.sql
@@ -0,0 +1,38 @@
+CREATE TABLE departments (
+	dept_no VARCHAR(10) PRIMARY KEY,
+	dept_name VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE dept_emp (
+	emp_no INT PRIMARY KEY,
+	dept_no VARCHAR(10) NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments(dept_no)
+);
+
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(10) PRIMARY KEY,
+	emp_no INT,
+	FOREIGN KEY (emp_no) REFERENCES dept_emp(emp_no)
+);
+
+CREATE TABLE titles (
+	title_id VARCHAR(10) PRIMARY KEY,
+	title VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE employees (
+	emp_no INT PRIMARY KEY,
+	emp_title_id VARCHAR(10),
+	FOREIGN KEY (emp_title_id) REFERENCES titles(title_id),
+	birth_date VARCHAR(10) NOT NULL,
+	first_name VARCHAR(30) NOT NULL,
+	last_name VARCHAR(30) NOT NULL,
+	sex VARCHAR(1) NOT NULL,
+	hire_date VARCHAR(30) NOT NULL
+);
+
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY,
+	FOREIGN KEY (emp_no) REFERENCES employees(emp_no),
+	salary INT
+);
