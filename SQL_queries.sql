afb2d39f7fc0390b8a9dd0cb4a1de7a59186fe5c
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-01 15:22:50 -0500

    Completed 8th query

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -55,3 +55,10 @@ INNER JOIN departments d
 ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales'
 OR d.dept_name = 'Development';
+
+
+-- In descending order, list the frequency count of employee last names, i.e., how many employees share each last name.
+SELECT last_name, COUNT(last_name) AS "Last Name Count"
+FROM employees
+GROUP BY last_name 
+ORDER BY "Last Name Count" DESC;
