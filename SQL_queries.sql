87184c7d78a5831afc13ec21ae0069913ad75182
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-01 15:11:18 -0500

    Completed 7th query

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -44,3 +44,14 @@ ON de.emp_no = e.emp_no
 INNER JOIN departments d
 ON d.dept_no = de.dept_no
 WHERE d.dept_name = 'Sales';
+
+
+-- List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de
+ON de.emp_no = e.emp_no
+INNER JOIN departments d
+ON d.dept_no = de.dept_no
+WHERE d.dept_name = 'Sales'
+OR d.dept_name = 'Development';
