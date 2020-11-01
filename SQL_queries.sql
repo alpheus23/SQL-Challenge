43410d1538ee3020d8880f0c6ae93a432bb31f4b
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-01 14:59:41 -0500

    Completed 4th query

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -18,3 +18,12 @@ INNER JOIN dept_emp de
 ON d.dept_no = de.dept_no
 INNER JOIN employees e
 ON e.emp_no = de.emp_no;
+
+
+-- List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.last_name, e.first_name, d.dept_name
+FROM employees e
+INNER JOIN dept_emp de
+ON de.emp_no = e.emp_no
+INNER JOIN departments d
+ON d.dept_no = de.dept_no;
