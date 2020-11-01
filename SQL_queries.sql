2368086b004c23da2aa2bf061cc36a826e1ded6f
Alpheus <69361507+alpheus23@users.noreply.github.com>
2020-11-01 14:41:59 -0500

    Completed second query

--- SQL_queries.sql
+++ SQL_queries.sql
@@ -2,4 +2,10 @@
 SELECT e.emp_no, e.last_name, e.first_name, e.sex, s.salary
 FROM salaries s
 INNER JOIN employees e
-ON s.emp_no = e.emp_no
+ON s.emp_no = e.emp_no;
+
+
+-- List first name, last name, and hire date for employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM employees 
+WHERE hire_date LIKE '%1986'
